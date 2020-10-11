b8417f9df0563cd46b18c45ce0f2647f8b30066b
Fadlan Fajar Ramdani <fdlnfjrrmdni@gmail.com>
2020-10-11 13:53:48 +0700

    task1

---
+++ day1/task1.go
@@ -0,0 +1,29 @@
+package main
+
+import ("fmt")
+
+func printImage(n int) string {
+	if n % 2 == 1 {
+		i := 0
+		for i < n {
+			j := 0
+			for j < n {
+				if j == 0 || i == n/2 || j == n-1 {
+					fmt.Print("*    ")
+				}else {
+					fmt.Print("=    ")
+				}
+				j++
+			}
+			fmt.Print("\n\n")
+			i++
+		}
+	}else {
+		fmt.Println("error: the value must be an odd number")
+	}
+	return ""
+}
+
+func main() {
+	printImage(5)
+}
